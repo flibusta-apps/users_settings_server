358b5eebb303939100d61caf43714bffe361cce6
Bulat Kurbanov <kurbanovbul@gmail.com>
2023-05-24 20:55:05 +0200

    Enable donation_notifications_router

--- src/app/views/__init__.py
+++ src/app/views/__init__.py
@@ -1,6 +1,12 @@
+from .donate_notifications import donation_notifications_router
 from .healthcheck import healthcheck_router
 from .languages import languages_router
 from .users import users_router
 
 
-__all__ = ["healthcheck_router", "languages_router", "users_router"]
+__all__ = [
+    "healthcheck_router",
+    "languages_router",
+    "users_router",
+    "donation_notifications_router",
+]

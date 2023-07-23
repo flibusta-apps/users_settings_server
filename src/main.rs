91d2fee0a7b8c2543d8f90829149d5b5014ad2f9
Bulat Kurbanov <kurbanovbul@gmail.com>
2023-07-23 22:53:29 +0200

    Fix

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,22 @@ pub mod prisma;
 pub mod views;
 pub mod db;
 
+use tracing::{info};
+
 use std::net::SocketAddr;
 
 
 async fn start_app() {
     let app = views::get_router();
 
-    // env_logger::init();
-
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
-    log::info!("Start webserver...");
+    info!("Start webserver...");
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
-    log::info!("Webserver shutdown...")
+    info!("Webserver shutdown...")
 }
 
 #[tokio::main]

5898d31809822cd25d83e0f6baf9b8ed76c0a5d4
Bulat Kurbanov <kurbanovbul@gmail.com>
2023-08-10 21:19:40 +0200

    Fix

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ pub mod prisma;
 pub mod views;
 pub mod db;
 
+use sentry::{ClientOptions, types::Dsn, integrations::debug_images::DebugImagesIntegration};
 use tracing::info;
 
-use std::net::SocketAddr;
+use std::{net::SocketAddr, str::FromStr};
 
 
 async fn start_app() {
@@ -28,7 +29,14 @@ async fn main() {
         .compact()
         .init();
 
-    let _guard = sentry::init(config::CONFIG.sentry_dsn.clone());
+    let options = ClientOptions {
+        dsn: Some(Dsn::from_str(&config::CONFIG.sentry_dsn).unwrap()),
+        default_integrations: false,
+        ..Default::default()
+    }
+    .add_integration(DebugImagesIntegration::new());
+
+    let _guard = sentry::init(options);
 
     start_app().await;
 }
